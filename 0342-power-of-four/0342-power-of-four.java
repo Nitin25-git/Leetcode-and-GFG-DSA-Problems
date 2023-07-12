5b18b33c4e146493c03dcf6f2e9f72f5212bb4bc
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2023-07-13 00:10:39 +0530

    Time: 1 ms (100.00%), Space: 39.6 MB (84.17%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==0){
+            return false;
+        } 
+        else if(n==1)
+        {
+            return true;
+        } 
+        else if(n%4==0 && isPowerOfFour(n/4)){
+            return true;
+        }
+        return false;
+    }
+}
