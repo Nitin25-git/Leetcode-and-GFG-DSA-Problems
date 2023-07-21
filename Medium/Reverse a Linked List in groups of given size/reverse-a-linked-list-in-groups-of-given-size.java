4aa8422a3ed38505eadf1fb928d1ebd09331b9e6
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2023-07-21 19:19:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse a Linked List in groups of given size/reverse-a-linked-list-in-groups-of-given-size.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args)throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = Integer.parseInt(s[i]);
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res,out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node node,PrintWriter out)
+    {
+        while(node != null)
+        {
+            out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node node, int k)
+    {
+        if(k==0 || k==1)
+          return node;
+          
+          Node current=node;
+          Node pvs=null;
+          Node temp=null;
+          int count=0;
+          while(current!=null && count<k)
+          {
+              temp=current.next;
+              current.next=pvs;
+              pvs=current;
+              current=temp;
+              count++;
+          }
+          if(current!=null)
+          node.next=reverse(current,k);
+          
+          return pvs;
+    }
+}
+
