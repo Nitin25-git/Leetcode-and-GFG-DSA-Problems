8f52c1a5f250111c3e585e293acdae7dde42183c
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2023-07-25 01:32:45 +0530

    Time: 0 ms (100.00%), Space: 41 MB (67.10%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n=digits.length-1;
-        for(int i=n;i>=0;i--)
+        int n=digits.length;
+        for(int i=digits.length-1;i>=0;i--)
         {
             if(digits[i]<9)
             {
-               digits[i]++;
+                digits[i]++;
                 return digits;
             }
             digits[i]=0;
+            
         }
-        digits=new int[digits.length+1];
-        digits[0]=1;
+        digits =new int[n+1];
+         digits[0]=1;
         return digits;
         
     }
