16b33567a77a6e08dcbb325a7ad50fdf31f68e30
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2023-08-20 09:58:31 +0530

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        int st=0;
+        int end=0;
+        ArrayList<Integer> list=new ArrayList<>();
+        int prefix=0;
+        for(int i=0;i<n;i++)
+        {
+            prefix=prefix+arr[i];
+            if(prefix>=s)
+            {
+                end=i;
+             while(prefix>s && st<end)   
+             {
+                 prefix=prefix-arr[st++];
+             }
+            }
+            if(prefix==s)
+            {
+                list.add(st+1);
+                list.add(end+1);
+                return list;
+            }
+        }
+        list.add(-1);
+        return list;
+    }
+}
