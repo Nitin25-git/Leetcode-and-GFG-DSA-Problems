62a6361e255c11261f34af911e981432423fc601
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2024-01-05 01:30:41 +0530

    Time: 15 ms (42.85%), Space: 48.4 MB (18.02%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,83 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        PriorityQueue<List<Integer>> pq=new PriorityQueue<>((a, b) -> a.get(1) - b.get(1));
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);
+            
+        }
+        
+        for(HashMap.Entry<Integer,Integer> entry:map.entrySet())
+        {
+            List<Integer> pair=new ArrayList<>();
+            pair.add(entry.getKey());
+            pair.add(entry.getValue());
+            
+            pq.offer(pair);
+            
+            if(pq.size()>k)
+            {
+                pq.poll();
+            }
+                
+        }
+        
+        int [] result=new int[k];
+        int index=0;
+        while(!pq.isEmpty())
+        {
+            result[index++]=pq.poll().get(0);
+        }
+        
+        return result;
+        
+        
+        
+    }
+}
+
+
+
+
+
+
+// import java.util.*;
+
+// class Solution {
+
+//     public int[] topKFrequent(int[] nums, int k) {
+//         // Min-heap
+//         PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a, b) -> a.get(1) - b.get(1));
+
+//         // Count frequency of each element
+//         // Worst Case - n distinct elements are stored, so, space O(n)
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int i : nums) {
+//             map.put(i, map.getOrDefault(i, 0) + 1);
+//         }
+
+//         // Push in min-heap and maintain size k
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             List<Integer> pair = new ArrayList<>();
+//             pair.add(entry.getKey());
+//             pair.add(entry.getValue());
+
+//             pq.offer(pair);
+
+//             if (pq.size() > k) {
+//                 pq.poll();
+//             }
+//         }
+
+//         // Pick all top K elements
+//         int[] result = new int[k];
+//         int index = 0;
+//         while (!pq.isEmpty()) {
+//             result[index++] = Objects.requireNonNull(pq.poll()).get(0);
+//         }
+//         return result;
+//     }
+// }
