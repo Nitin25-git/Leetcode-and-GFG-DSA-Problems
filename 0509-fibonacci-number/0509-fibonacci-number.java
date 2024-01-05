341734d9fc4047a59d0d16aa24b7d9f4482d9e8c
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2024-01-05 15:35:01 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (5.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,23 @@
 class Solution {
     public int fib(int n) {
-       if(n==0 || n==1)
-           return n;
         
-        return fib(n-1)+fib(n-2);
+        int arr[]=new int[31];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=-1;
+        }
+        return solve(n,arr);
+    }
+    
+    public int solve(int n,int arr[])
+    {
+        if(n<=1)
+            return n;
         
+        if(arr[n]!=-1)
+            return arr[n];
         
-        
-        
+        arr[n]=solve(n-1,arr)+solve((n-2),arr);
+        return arr[n];
     }
 }
