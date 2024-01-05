60085014491435837fb2365ff4810811ca9a925c
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2024-01-05 17:37:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (5.55%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int dp[]=new int[nums.length+1];
+        
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<dp.length;i++)
+        {
+            int steal=nums[i-1]+dp[i-2];
+            int skip=dp[i-1];
+            
+            dp[i]=Math.max(skip,steal);
+        }
+        
+        return dp[dp.length-1];
+        
+    }
+}
