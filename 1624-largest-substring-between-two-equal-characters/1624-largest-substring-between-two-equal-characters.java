ad750010aef8b85659a83e8f25247a0647350f5c
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2024-02-09 16:21:56 +0530

    Time: 7 ms (27.67%), Space: 40.9 MB (88.59%) - LeetHub

---
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int max=-1;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            for(int j=i+1;j<s.length();j++)
+            {
+                if(s.charAt(i)==s.charAt(j))
+                {
+                    max=Math.max(j-i-1,max);
+                }
+            }
+        }
+        
+        return max;
+        
+    }
+}
