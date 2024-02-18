ba0798e6102fd75c2ab47db89a2f255e57ce6832
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2024-02-19 01:34:08 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (97.11%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,9 +10,8 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head==null || head.next==null)
-            return head;
-        
+        if(head==null && head.next==null)
+            return null;
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null)
@@ -22,5 +21,6 @@ class Solution {
         }
         
         return slow;
+        
     }
 }
