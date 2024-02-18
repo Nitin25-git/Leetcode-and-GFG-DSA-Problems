a8139fc456949cc2e8a5281452d09cd57702bf3d
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2024-02-19 01:45:27 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (42.87%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -13,11 +13,9 @@ class Solution {
         if(head==null || head.next==null || head.next.next==null)
             return head;
         ListNode evenhead=head.next;
-                ListNode evenhead1=head.next;
-
         ListNode oddhead=head;
         
-        
+        ListNode evenhead1=head.next;
         while(evenhead!=null && evenhead.next!=null)
         {
             oddhead.next=evenhead.next;
@@ -27,6 +25,7 @@ class Solution {
         }
         oddhead.next=evenhead1;
         return head;
+        
         
     }
 }
