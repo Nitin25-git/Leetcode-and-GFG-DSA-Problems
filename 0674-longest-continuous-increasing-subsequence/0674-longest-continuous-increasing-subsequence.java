01ce18580ca1d6e98cad9f84e1ab42bda0e6fe53
Nitin Kushwaha <83164790+Nitin25-git@users.noreply.github.com>
2024-02-27 22:29:29 +0530

    Time: 1 ms (99.63%), Space: 44.3 MB (58.15%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
@@ -3,30 +3,31 @@ class Solution {
         if(nums.length<=1)
             return nums.length;
         
-        int n=nums.length;
-        boolean increasing=false;
+         int n=nums.length;
+        // boolean increasing=false;
         
-        int start=0;
-        int end=0;
+        // int start=0;
+        // int end=0;
+        int c=1;
         int max=Integer.MIN_VALUE;
         for(int i=0;i<n-1;i++)
         {
             
             if(nums[i]<nums[i+1])
             {
-                increasing=true;
-                end=i+1;
+                c++;
+                max=Math.max(c,max);
+
             }
             else
             {
-                start=i+1;
+                c=1;
                 
             }
-                            max=Math.max(end-start+1,max);
 
                 
         }
-        return increasing?max:1;
+        return max>0?max:1;
         
     }
 }
